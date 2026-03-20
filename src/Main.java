f84816ffc06f1cf6d72ef6a8fed88c9d8c088988
Viniciusant-souz <viniciusantonio.ss@hotmail.com>
2026-03-20 14:02:20 -0300

    Feat: Variáveis e Tipos de Dados

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,23 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Meu primeiro program em Java!");
-        System.out.println("Meu nome é Vinícius");
+        int idade = 30;
+        double altura = 1.65;
+        boolean estudando = true;
+        String nome = "Vinícius";
+        String cidade = "Platina/SP";
+        double salario = 12000;
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+        System.out.println("Estudando: " + estudando);
+        System.out.println("Eu mora na cidade de " + cidade + ", e o salário desejado é R$" + salario);
+
+
+
+
+
+
         }
     }
