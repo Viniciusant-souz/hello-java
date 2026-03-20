e08b7f529f27ea327a2451ecd412f483c6437c14
Viniciusant-souz <viniciusantonio.ss@hotmail.com>
2026-03-20 14:30:52 -0300

    feat: trabalhando com operadores lógicos

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,31 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int idade = 30;
-        double alt = 1.65;
-        boolean estudando = true;
-        String nome = "Vinícius";
-        String cid = "Platina/SP";
-        double salario = 12000;
+       int a = 10;
+       int b = 3;
 
-        System.out.println("Nome: " + nome);
-        System.out.println("Idade: " + idade);
-        System.out.println("Altura: " + alt);
-        System.out.println("Estudando: " + estudando);
-        System.out.println("Eu mora na cidade de " + cid + ", e o salário desejado é R$" + salario);
+       int soma = a + b;
+       int subtracao = a - b;
+       int divisao = a / b;
+       int multiplicacao = a * b;
+       int resto = a % b;
+       double divisaoDecimal = (double) a / b;
+
+       double peso = 70.0;
+       double altura = 1.60;
+       double imc = (double) peso / (double) (altura*altura);
+
+        System.out.println("Soma: " + soma);
+        System.out.println("Subtrção: " + subtracao);
+        System.out.println("Divisão: " + divisao);
+        System.out.println("Multiplicação: " + multiplicacao);
+        System.out.println("Resto: " + resto);
+        System.out.println("Divisão Decimal: " + divisaoDecimal);
+
+        System.out.println("Meu IMC é: " + imc);
+
+
+
 
 
 
