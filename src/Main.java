c97beb9e79ee78d90e207a08d84122b31702a334
Viniciusant-souz <viniciusantonio.ss@hotmail.com>
2026-03-25 14:13:19 -0300

    feat: sistema de acesso com bloqueio e 4 condições

--- src/Main.java
+++ src/Main.java
@@ -3,24 +3,31 @@
 public class Main {
     public static void main (String[] args) {
 
-        String usuario = "adminn";
-        String senha = "12345";
+        String usuario = "admin";
+        String senha = "1234";
+        int tentativas = 3;
 
-        if (usuario.equals("admin") && senha.equals("1234")){
+        if (tentativas >=3) {
+            System.out.println("ACESSO BLOQUEADO POR EXCESSO DE TENTATIVAS");
+        }
+
+        else if (usuario.equals("admin") && senha.equals("1234")) {
             System.out.println("Acesso autorizado para o usuário " + usuario);
         }
 
-        else if (!usuario.equals("admin") && !senha.equals("1234")){
-            System.out.println("Usuário e Senha Incorretos ");
+        else if (usuario.equals("admin")){
+            System.out.println("Senha Incorreta. Tentativas: " + tentativas);
         }
 
-        else if (usuario.equals("admin")) {
-            System.out.println("Senha Incorreta");
+        else if (senha.equals("1234")) {
+            System.out.println("Usuário Incorreto. Tentavias: " + tentativas);
+        }
 
+        else if (!senha.equals("1234") && !usuario.equals("admin")) {
+            System.out.println("Usuário e Senha Incorretos. Tentativas: " + tentativas);
         }
-        else{
-            System.out.println("Usuário incorreto");
-        }
+
+
 
     }
 }
